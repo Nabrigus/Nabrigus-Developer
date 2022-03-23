1a397f033332546c61a201bd38fdfe8e464998bd
Fabricio Nicolas Diarte <75744224+Nabrigus@users.noreply.github.com>
2022-03-23 14:20:35 -0300

    Add files via upload

--- Web-portfolio/JS/Change.js
+++ Web-portfolio/JS/Change.js
@@ -7,22 +7,24 @@
 $('.images_nav2').bind('click',()=>{$('.list_nav').toggleClass('mostrar');})
 
 /*Scroll Menu de Navegacion*/
-var ancho = window.screen.width;
-console.log(ancho);
-if(ancho > 800){
+var alto = window.screen.height;
+console.log(alto);
+if(alto > 800){
     
     $('#inicio').bind('click',()=>{ window.scroll({top:0,behavior:'smooth'});})
     $('#sobre').bind('click',()=>{ window.scroll({top:1000,behavior:'smooth'})})
     $('#tecnologias').bind('click',()=>{ window.scroll({top:2000,behavior:'smooth'})})
     $('#contacto').bind('click',()=>{ window.scroll({top:2500,behavior:'smooth'})})
 }
-if(ancho < 800){
+
+if(alto < 800){
             
     $('#inicio').bind('click',()=>{ window.scroll({top:0,behavior:'smooth'});})
-    $('#sobre').bind('click',()=>{ window.scroll({top:1000,behavior:'smooth'})})
-    $('#tecnologias').bind('click',()=>{ window.scroll({top:2000,behavior:'smooth'})})
+    $('#sobre').bind('click',()=>{ window.scroll({top:740,behavior:'smooth'})})
+    $('#tecnologias').bind('click',()=>{ window.scroll({top:1400,behavior:'smooth'})})
     $('#contacto').bind('click',()=>{ window.scroll({top:3000,behavior:'smooth'})})
 }
+
 
 
 
